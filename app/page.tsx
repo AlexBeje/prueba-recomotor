563f20418bdc6f07eb6dc0b33627fb65c894f579
AlexBeje <alex.bejenaru93@gmail.com>
2024-08-02 17:15:57 +0200

    📦 NEW: Added favorites example.

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,70 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [cars, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      const response = await fetch("https://recomotor-back.alexbeje.dev/cars");
-      const data = await response.json();
-      setUsers(data);
-    };
     fetchUsers();
   }, []);
 
+  const fetchUsers = async () => {
+    const response = await fetch("https://recomotor-back.alexbeje.dev/users");
+    const data = await response.json();
+    setUsers(data);
+  };
+
+  const removeFavorite = async () => {
+    const body = {
+      favorites: [],
+    };
+    console.log("🤴", JSON.stringify(body));
+    await fetch(
+      "https://recomotor-back.alexbeje.dev/users/66ac6b5ef4def290859e3042",
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          favorites: [
+            {
+              id: 1,
+              brand: "Seat",
+              model: "Leon",
+            },
+            {
+              id: 2,
+              brand: "Mercedes-Ben",
+              model: "B Class",
+            },
+          ],
+        }),
+      }
+    );
+    fetchUsers();
+  };
+
   return (
-    <div>
-      {/* <button onClick={updateFavorites}>Update favorites</button> */}
-      <ul>
-        {cars.map((car: any, id) => (
-          <li key={id}>{car.brand}</li>
-        ))}
-      </ul>
+    <div className="p-4">
+      <h1 className="text-4xl mb-4">Favorites</h1>
+      {users.map((user: any, id) => (
+        <div key={id}>
+          <h1 className="text-3xl">{user.name}</h1>
+          <ul className="text-sm list-disc list-inside">
+            {user.favorites.map((favorite: any) => (
+              <li key={id}>
+                {favorite.brand} {favorite.model}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      {/* <button
+        onClick={removeFavorite}
+        className="mt-2 p-2 bg-blue-500 text-white"
+      >
+        Remove favorite
+      </button> */}
     </div>
   );
 }
