4cd69d410f6e33b9f7a69448a2f614cd416fe344
AlexBeje <alex.bejenaru93@gmail.com>
2024-08-02 15:43:48 +0200

    📦 NEW: Added nextjs vercel cors config.

--- app/page.tsx
+++ app/page.tsx
@@ -4,65 +4,26 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [users, setUsers] = useState([]);
-  const [cars, setCars] = useState([]);
+  // const [cars, setCars] = useState([]);
 
   useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch(
+        "https://recomotor-back.alexbeje.dev/users",
+        {
+          method: "GET",
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            Accept: "application/json",
+          },
+        }
+      );
+      console.log("👶", response);
+      const data = await response.json();
+      setUsers(data);
+    };
     fetchUsers();
-    fetchCars();
   }, []);
 
-  const fetchUsers = async () => {
-    const response = await fetch("https://recomotor-back.alexbeje.dev/users");
-    const data = await response.json();
-    console.log("🏃‍♀️", data);
-    setUsers(data);
-  };
-
-  const fetchCars = async () => {
-    const response = await fetch("https://recomotor-back.alexbeje.dev/cars");
-    const data = await response.json();
-    setCars(data);
-  };
-
-  const updateFavorites = async () => {
-    const response = await fetch(
-      "https://recomotor-back.alexbeje.dev/users/66ac6b5ef4def290859e3042",
-      {
-        method: "PATCH",
-        body: JSON.stringify({
-          favorites: [
-            {
-              id: 1,
-              brand: "Seat",
-              model: "Leon",
-            },
-            {
-              id: 2,
-              brand: "Nissan",
-              model: "Juke",
-            },
-          ],
-        }),
-      }
-    );
-    const data = await response.json();
-    console.log("🖐", data);
-  };
-
-  return (
-    <div>
-      home
-      <ul>
-        {users.map((user) => (
-          <li>{user.name}</li>
-        ))}
-      </ul>
-      <ul>
-        {cars.map((car) => (
-          <li>{car.brand}</li>
-        ))}
-      </ul>
-      <button onClick={updateFavorites}>Update favorites</button>
-    </div>
-  );
+  return <div>home</div>;
 }
