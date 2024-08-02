e62f44b1e05eedcbe0f26169ac590b3d889c4228
AlexBeje <alex.bejenaru93@gmail.com>
2024-08-02 15:01:27 +0200

    📦 NEW: Added new getUsers fetch method.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,46 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function Home() {
-  return <p>home</p>;
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://recomotor-back.alexbeje.dev/users");
+      const data = await response.json();
+      console.log("🖐", data);
+    };
+    fetchData();
+  }, []);
+
+  const updateFavorites = async () => {
+    const response = await fetch(
+      "https://recomotor-back.alexbeje.dev/users/66ac6b5ef4def290859e3042",
+      {
+        method: "PATCH",
+        body: JSON.stringify({
+          favorites: [
+            {
+              id: 1,
+              brand: "Seat",
+              model: "Leon",
+            },
+            {
+              id: 2,
+              brand: "Nissan",
+              model: "Juke",
+            },
+          ],
+        }),
+      }
+    );
+    const data = await response.json();
+    console.log("🖐", data);
+  };
+
+  return (
+    <div>
+      home
+      <button onClick={updateFavorites}>Update favorites</button>
+    </div>
+  );
 }
--- pages/cars.tsx
+++
@@ -1,9 +0,0 @@
-import { getUsers } from "@/app/actions";
-import { useEffect } from "react";
-
-export default function Cars() {
-  useEffect(() => {
-    getUsers();
-  }, []);
-  return "test";
-}
